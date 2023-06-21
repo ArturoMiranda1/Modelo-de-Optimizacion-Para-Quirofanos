4b473b605c70216c62c019d196116a0ef688ac89
ArturoMiranda1 <arturomirandaa0510@gmail.com>
2023-06-21 09:32:36 -0500

    terminando interfaz

--- Modelo de optimizacion/Conexion.py
+++ Modelo de optimizacion/Conexion.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import pymysql
+from datetime import datetime, timedelta
 
 # Conexión a la base de datos
 conexion = pymysql.connect(
@@ -12,7 +13,7 @@ cursor = conexion.cursor()
 
 # Crear ventana
 ventana = tk.Tk()
-ventana.title("API Base de Datos")
+ventana.title("Interfaz Cirugias")
 
 # Dimensiones de la ventana
 ancho_ventana = 500
@@ -33,6 +34,8 @@ ventana.geometry(f"{ancho_ventana}x{alto_ventana}+{x_pos}+{y_pos}")
 especialidad_seleccionada = tk.StringVar()
 medico_seleccionado = tk.StringVar()
 cirugia_seleccionada = tk.StringVar()
+hora_inicio = tk.StringVar()
+hora_fin = tk.StringVar()
 
 # Función para obtener los médicos según la especialidad seleccionada
 def obtener_medicos():
@@ -56,7 +59,7 @@ def obtener_medicos():
     cuadro_cirugias.delete(0, tk.END)
     cirugia_seleccionada.set("")
 
-
+# Función para obtener las cirugías de la especialidad seleccionada
 def obtener_cirugias():
     especialidad = especialidad_seleccionada.get()
 
@@ -74,6 +77,26 @@ def obtener_cirugias():
     # Limpiar cuadro de cirugia seleccionada
     cirugia_seleccionada.set("")
 
+# Función para calcular la hora de finalización
+def calcular_hora_fin():
+    seleccion = cuadro_cirugias.curselection()
+    if seleccion:
+        indice = int(seleccion[0])
+        nombre_cirugia = cuadro_cirugias.get(indice)
+        consulta_duracion = "SELECT duracion_horas, duracion_minutos FROM cirugias WHERE nombre_cirugia = %s"
+        cursor.execute(consulta_duracion, (nombre_cirugia,))
+        duracion = cursor.fetchone()
+        duracion_horas = int(duracion[0])
+        duracion_minutos = int(duracion[1])
+
+        hora_inicio_str = hora_inicio.get()
+        try:
+            hora_inicio_dt = datetime.strptime(hora_inicio_str, "%H:%M")
+            hora_fin_dt = hora_inicio_dt + timedelta(hours=duracion_horas, minutes=duracion_minutos)
+            hora_fin_str = hora_fin_dt.strftime("%H:%M")
+            hora_fin.set(hora_fin_str)
+        except ValueError:
+            hora_fin.set("Error")
 
 # Obtener las especialidades desde la base de datos
 consulta_especialidades = "SELECT DISTINCT especialidad FROM EE"
@@ -110,6 +133,21 @@ cuadro_cirugias.pack()
 cuadro_cirugia_seleccionada = tk.Entry(ventana, textvariable=cirugia_seleccionada, width=50, state="readonly")
 cuadro_cirugia_seleccionada.pack()
 
+# Etiqueta y cuadro de selección de hora de inicio
+etiqueta_hora_inicio = tk.Label(ventana, text="Hora de Inicio:")
+etiqueta_hora_inicio.pack()
+cuadro_hora_inicio = tk.Entry(ventana, textvariable=hora_inicio)
+cuadro_hora_inicio.pack()
+
+# Etiqueta y cuadro de hora de fin
+etiqueta_hora_fin = tk.Label(ventana, text="Hora de Finalización:")
+etiqueta_hora_fin.pack()
+cuadro_hora_fin = tk.Entry(ventana, textvariable=hora_fin, state="readonly")
+cuadro_hora_fin.pack()
+
+# Botón para calcular la hora de finalización
+boton_calcular_hora_fin = tk.Button(ventana, text="Agendar", command=calcular_hora_fin)
+boton_calcular_hora_fin.pack()
 
 # Asignar el nombre de la cirugía seleccionada al cuadro de texto
 def seleccionar_cirugia(event):
@@ -121,7 +159,6 @@ def seleccionar_cirugia(event):
 
 cuadro_cirugias.bind("<<ListboxSelect>>", seleccionar_cirugia)
 
-
 # Asignar el nombre del médico seleccionado al cuadro de texto
 def seleccionar_medico(event):
     widget = event.widget
@@ -132,10 +169,10 @@ def seleccionar_medico(event):
 
 cuadro_medicos.bind("<<ListboxSelect>>", seleccionar_medico)
 
-
 # Ejecutar interfaz
 ventana.mainloop()
 
 # Cerrar conexión a la base de datos al cerrar la ventana
 cursor.close()
 conexion.close()
+
